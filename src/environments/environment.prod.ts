88d075e1243ef8d3809e6020138b932fc012b703
francisco <fr.betancourt@live.com>
2020-11-23 23:15:48 -0300

    Corregido variables entorno producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  rework_backend_url: '',
+  rework_backend_url: 'https://rework-dev.herokuapp.com/api',
   stripe_pk: 'pk_live_51Hj9kwAR2u98Fmdnybb3IXp1EbwrBGWNyLcZk747hFyIVcMAeSZStXRXOBV8v3F8vL8zZ1SAd4hDKZTXHHoahKH300HsCpPHRI'
 };
