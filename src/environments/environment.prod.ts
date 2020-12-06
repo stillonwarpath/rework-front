04fba42e77339d4ffd56a7b0e176e91fde3f2f6f
francisco <fr.betancourt@live.com>
2020-12-05 21:10:31 -0300

    Añadidas variables de entorno producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   post_free: true,
   rework_backend_url: 'https://rework-prod.herokuapp.com/api',
   stripe_pk: 'pk_live_51Hj9kwAR2u98Fmdnybb3IXp1EbwrBGWNyLcZk747hFyIVcMAeSZStXRXOBV8v3F8vL8zZ1SAd4hDKZTXHHoahKH300HsCpPHRI',
-  max_file_size: 2621440
+  max_file_size: 2621440,
+  digital_ocean_spaces:'https://altalineastudio.nyc3.digitaloceanspaces.com/rework/uploads/post'
 };
